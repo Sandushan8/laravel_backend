9b8f89d163ee01fd85df20e85422fb56178f631b
Sandushan8 <dewpurai@gmail.com>
2024-09-01 01:53:44 +0530

    Crud and login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\RegisterController;
+
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+     
+Route::middleware('auth:api')->group( function () {
+    Route::resource('products', ProductController::class);
+});
